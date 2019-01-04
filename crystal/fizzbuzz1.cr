6778bab1ca05d62a624930b0c0e0518e9bb11a7e
George Stavrinos <gstavrinos@iit.demokritos.gr>
2019-01-04 14:00:06 +0200

    Added crystal example

---
+++ crystal/fizzbuzz1.cr
@@ -0,0 +1,16 @@
+
+(0..100).each do |i|
+    fzbz = false
+    if i % 3 == 0
+        fzbz = true
+        print "Fizz"
+    end
+    if i % 5 == 0
+        fzbz = true
+        print "Buzz"
+    end
+    if !fzbz
+        print i
+    end
+    puts
+end

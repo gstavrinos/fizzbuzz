631dbc63f6ecddd69351b75cd4b9f7bdcaaa6c7e
George Stavrinos <gstavrinos@iit.demokritos.gr>
2019-01-05 18:37:57 +0200

    Added a perl6 example

---
+++ perl6/fizzbuzz1.pl
@@ -0,0 +1,16 @@
+
+for 0..100 {
+    my $fzbz = False;
+    if $_ % 3 == 0 {
+        $fzbz = True;
+        print "Fizz"
+    }
+    if $_ % 5 == 0 {
+        $fzbz = True;
+        print "Buzz"
+    }
+    if !$fzbz {
+        print $_;
+    }
+    print "\n";
+}
